00a4af89081ce437db444474f736c4ebc2d4b3f3
jchri772 <chrisju77@uchicago.edu>
2026-03-04 16:04:37 -0600

    update

--- pages/By US Airport.py
+++ pages/By US Airport.py
@@ -75,7 +75,7 @@ def render_airport_page():
     years_bar_chart = alt.Chart(pax_by_airport).mark_bar().add_params(selection_year).encode(
         x=alt.X('YEAR:O', title="Year"),
         y=alt.Y('sum(PASSENGERS):Q', title="Total Passengers", 
-                axis=alt.Axis(titlePadding=10, labelPadding=20, minExtent=80)),
+                axis=alt.Axis(titlePadding=10, format='.2s', minExtent=80)),
         opacity=alt.condition(
             selection_year, alt.value(1.0), alt.value(0.3)),
         tooltip=[
@@ -155,7 +155,7 @@ def render_airport_page():
 
     line_graph_by_airport = alt.Chart(full_airport_map).mark_line(point=True).encode(
         y=alt.Y('sum(PASSENGERS):Q', title='Sum of International Passengers', 
-                axis=alt.Axis(titlePadding=10, labelPadding=20, minExtent=80)),
+                axis=alt.Axis(titlePadding=10, format='.2s', minExtent=80)),
         x=alt.X('YEAR:O', title='Year'),
         tooltip=[
             alt.Tooltip('YEAR:O', title='Year'),
