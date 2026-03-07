11de22604f505a7d363ceadfbc10866c306755d4
jchri772 <chrisju77@uchicago.edu>
2026-03-06 19:36:55 -0600

    update

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+st.markdown(
+    """
+    <style>
+        /* Replace the text of the first link in the sidebar */
+        [data-testid="stSidebarNav"] ul li:first-child a span {
+            visibility: hidden;
+        }
+        [data-testid="stSidebarNav"] ul li:first-child a span::after {
+            content: "Homepage";
+            visibility: visible;
+            display: block;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("US International Air Travel Dashboard (1990-2025)")
 
 st.markdown("""
