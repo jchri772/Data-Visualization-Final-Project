dca96ffce120effeabab0c5e160d514791d7b161
evanli7334 <evanli@uchicago.edu>
2026-03-08 18:05:51 -0500

    Update 05 Conclusion.py

--- pages/05 Conclusion.py
+++ pages/05 Conclusion.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import base64
 
+st.markdown(
+    """
+    <style>
+    header {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 def set_background(image_file):
     with open(image_file, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
