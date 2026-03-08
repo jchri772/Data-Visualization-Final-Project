be1f6fc1681d1d232d68308746757fff4d0fe9de
evanli7334 <evanli@uchicago.edu>
2026-03-08 18:06:33 -0500

    Update 05 Conclusion.py

--- pages/05 Conclusion.py
+++ pages/05 Conclusion.py
@@ -1,34 +1,4 @@
 import streamlit as st
-import base64
-
-st.markdown(
-    """
-    <style>
-    header {visibility: hidden;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-def set_background(image_file):
-    with open(image_file, "rb") as f:
-        encoded = base64.b64encode(f.read()).decode()
-
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/png;base64,{encoded}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-set_background("background.jpg")
 
 st.title("Conclusion")
 
