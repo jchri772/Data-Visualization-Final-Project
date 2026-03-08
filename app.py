54243b556d908b39388f12256bebbb37929d122d
evanli7334 <evanli@uchicago.edu>
2026-03-07 18:33:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
+st.markdown(
+    """
+    <style>
+    header {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 def set_background(image_file):
     with open(image_file, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
